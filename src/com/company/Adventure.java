890b4fa018777a814ed0960fce5a1401dd4e3d7b
mms <mark425e@stud.kea.dk>
2021-09-24 16:22:59 +0200

    commit

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -11,7 +11,7 @@ public class Adventure {
         Room Room2 = new Room(null,null,null, null, "The valley");
         Room Room3 = new Room(null,null,null,null , "The clearing");
         Room Room4 = new Room(null,null,null,null, "Abandoned house");
-        Room Room5 = new Room(null,null,null,null, "Room 5");
+        Room Room5 = new Room(null,null,null,null, "The statue");
         Room Room6 = new Room(null,null,null,null, "The old battlefield");
         Room Room7 = new Room(null,null,null,null, "Bridge");
         Room Room8 = new Room(null,null,null,null, "Monuments");
@@ -42,21 +42,23 @@ public class Adventure {
         //Beskriv rum
         Room1.setRoomDesc("You are in a quiet town on the edge of the forest. The town is surrounded by mountains to the north and west");
         Room2.setRoomDesc("You are standing a lush valley full of flowers. A large river runs to the south");
-        Room3.setRoomDesc("You enter a clearing in the woods. Seems like somebody made camp here. A red banner hangs from a tree .The forest to the east seems very thick");
+        Room3.setRoomDesc("You enter a clearing in the woods. Seems like somebody made camp here. A red banner hangs from a tree. The forest to the east seems very thick");
         Room4.setRoomDesc("A small wooden house appears. It seems to be abandoned. A large river flow to the east");
-        Room5.setRoomDesc("You see a large stone structure in front of you");
+        Room5.setRoomDesc("You see a large stone statue in front of you. It has a diamond shaped hole in its base");
         Room6.setRoomDesc("You stumble upon several corpses. A bloody battle has taken place here. There is a lot of debris to the west");
         Room7.setRoomDesc("You see a bridge over the river. The forest seems to be thinning out to the south and west");
         Room8.setRoomDesc("There two monuments to the north forming a gate. Ancient runes decorate them");
-        Room9.setRoomDesc("There are small tents arranged around a fire place. A Blue banner hangs from a tree.");
+        Room9.setRoomDesc("There are small tents arranged around a fire place. A corpse in blue uniform lays on the ground clutching something.");
 
 
         //Scanner til input
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hey welcome to the Adventure game. " +
-                "You are trying to find the secret treasure hidden in the forest\n"+
-                "The forest is know to be dangerous so keep on your guard.\n\n");
+        System.out.println("""
+                Hey welcome to the Adventure game. You are trying to find the secret treasure hidden in the forest
+                The forest is know to be dangerous so keep on your guard.
+
+                """);
         System.out.println(Room1.getRoomDesc());
 
         //loop til at holde spillet kørende og boolean til at afbryde spillet
